fae9f8894ea05be487fa6933dafe76279c526d3d
Kai Hatje <kai.hatje@app-akademie.com>
2024-09-05 12:17:51 +0200

    Implemented login functionality.

--- main.dart
+++ main.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
 
+// Login, damit der Fortschritt online gespeichert wird.
+// Anzeigen von Code Snippets, bei denen gesagt werden soll, was rauskommt.
+// Ein Exerciseboard, auf dem man sieht, wie viele und welche Aufgaben man gemacht hat.
+
 void main() {
   // Login, damit der Fortschritt online gespeichert wird.
   // Anzeigen von Code Snippets, bei denen gesagt werden soll, was rauskommt.
@@ -7,11 +11,18 @@ void main() {
   print("Willkommen bei Dart Fast CLI.");
   print(
       "Hier wird dir Code gezeigt, von dem du sagen sollst, ob er richtig oder falsch ist.");
+
+  bool isUserLoggedIn = false;
+
   // Wie lange läuft das Programm?
   bool isProgramRunning = true;
   while (isProgramRunning) {
-    print(
-        "Was möchtest du tun? Programm (b)eenden, (L)ogin, (S)nippet anzeigen.");
+    if (isUserLoggedIn) {
+      print("Was möchtest du tun? Programm (b)eenden, (S)nippet anzeigen.");
+    } else {
+      print(
+          "Was möchtest du tun? Programm (b)eenden, (L)ogin, (S)nippet anzeigen.");
+    }
     // Eingabe des Benutzers.
     String userChoiceInput = stdin.readLineSync()!;
 
@@ -20,10 +31,33 @@ void main() {
         isProgramRunning = false;
       // Benutzer soll sich mit Username und Passwort einloggen können.
       case "l" || "L":
+        // Benutzername und Passwort abfragen.
+        // Schauen, ob es mit gespeicherten Daten übereinstimmt.
+        // Wenn ja, dann ist Benutzer eingeloggt.
+        // Wenn nein, entsprechende Ausgabe machen.
+        String expectedUserName = "kai";
+        String expecedUserPassword = "passwort";
+
+        String inputUserName = "";
+        String inputUserPassword = "";
+
+        // Benutzername und Passwort abfragen.
         stdout.write("Bitte gib deinen Benutzername ein: ");
-        stdin.readLineSync()!;
+        inputUserName = stdin.readLineSync()!;
         stdout.write("Bitte gib dein Passwort ein: ");
-        stdin.readLineSync();
+        inputUserPassword = stdin.readLineSync()!;
+
+        // Schauen, ob es mit gespeicherten Daten übereinstimmt.
+        bool isLoginDataCorrect = inputUserName == expectedUserName &&
+            inputUserPassword == expecedUserPassword;
+
+        if (isLoginDataCorrect) {
+          print("Du hast dich erfolgreich eingeloggt");
+          isUserLoggedIn = true;
+        } else {
+          print("Du hast leider deine Daten falsch eingegeben");
+          isUserLoggedIn = false;
+        }
 
         print("");
       case "s" || "S":
