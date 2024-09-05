606fddf26c7b832fe5e7cc9f09c5aedd3c3771a4
Kai Hatje <kai.hatje@app-akademie.com>
2024-09-05 12:34:11 +0200

    Programmed showing one small exercise.

--- main.dart
+++ main.dart
@@ -60,10 +60,31 @@ void main() {
         }
 
         print("");
+      // Anzeigen von Code Snippets, bei denen gesagt werden soll, was rauskommt.
       case "s" || "S":
-        print("Was gibt der folgende Code aus?");
-        print('void main() { print("Hallo Batch 7")}');
-        print("");
+        // Überprüfen, ob der Benutzer eingeloggt ist.
+        // Wenn nein, Meldung ausgeben.
+        // Wenn ja, dann Code Snippet zeigen.
+        // Abfrage machen, was rauskommt.
+        // Je nach Abfrage loben oder noch mal zeigen.
+        String exercise = 'void main() { print("Hi")}';
+        String solution = '"Hi"';
+        bool exerciseSolved = false;
+
+        if (isUserLoggedIn) {
+          print("Was gibt der folgende Code aus?");
+          print(exercise);
+          print("");
+          String result = stdin.readLineSync()!;
+          if (result == solution) {
+            print("Sehr gut! Schön gelöst!");
+            exerciseSolved = true;
+          } else {
+            print("Versuche es noch mal, bitte");
+          }
+        } else {
+          print("Bitte einloggen!");
+        }
     }
 
     // Anzeige abhängig von Eingabe.
